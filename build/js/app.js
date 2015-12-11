8f51116c187b267fe72f8c3439f6042d9751ddd5
Cato Skogholt <cskogholt@gmail.com>
2015-12-11 19:22:35 +0100

    Build++

--- build/js/app.js
+++ build/js/app.js
@@ -87,17 +87,17 @@
 	angular.module('blocks.alert', []);
 })();
 (function() {
+	'use strict';
+
+	angular.module('blocks.sawkit', []);
+})();
+(function() {
     'use strict';
 
     angular
         .module('blocks.tokenIdentity', []);
 })();
 
-(function() {
-	'use strict';
-
-	angular.module('blocks.sawkit', []);
-})();
 (function() {
     'use strict';
 
@@ -112,11 +112,11 @@
         ApplicationName: 'Cato Skogholt Application',
         ApplicationPrefix: 'CSA',
 
-        // ApiUrl: 'https://blooming-eyrie-6843.herokuapp.com/',
-        // SocketUrl: 'https://blooming-eyrie-6843.herokuapp.com/',
+        ApiUrl: 'https://blooming-eyrie-6843.herokuapp.com/',
+        SocketUrl: 'https://blooming-eyrie-6843.herokuapp.com/',
 
-        ApiUrl: 'http://localhost:7203/',
-        SocketUrl: 'http://localhost:7203/'
+        // ApiUrl: 'http://localhost:7203/',
+        // SocketUrl: 'http://localhost:7203/'
     };
 
     angular
@@ -993,6 +993,55 @@
 		}
 	}
 })();
+/* global io */
+(function () {
+        'use strict';
+
+	angular
+		.module('blocks.sawkit')
+		.factory('sawkit', sawkit);
+
+	sawkit.$inject = ['$rootScope', 'appSettings'];
+	function sawkit($rootScope, appSettings) {
+
+		var socket;
+
+		var service = {
+            connect: connect,
+			on: on,
+			emit: emit
+		};
+
+        return service;
+
+        function connect(room) {
+             socket = io.connect(appSettings.SocketUrl + room);
+             console.log('sawkit is connecting to room: ' + room);
+        }
+
+        function on(eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        }
+
+        function emit(eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            });
+        }
+
+	}
+})();
+
 (function() {
     'use strict';
 
@@ -1081,55 +1130,6 @@
         }
 
     }
-})();
-
-/* global io */
-(function () {
-        'use strict';
-
-	angular
-		.module('blocks.sawkit')
-		.factory('sawkit', sawkit);
-
-	sawkit.$inject = ['$rootScope', 'appSettings'];
-	function sawkit($rootScope, appSettings) {
-
-		var socket;
-
-		var service = {
-            connect: connect,
-			on: on,
-			emit: emit
-		};
-
-        return service;
-
-        function connect(room) {
-             socket = io.connect(appSettings.SocketUrl + room);
-             console.log('sawkit is connecting to room: ' + room);
-        }
-
-        function on(eventName, callback) {
-            socket.on(eventName, function () {
-                var args = arguments;
-                $rootScope.$apply(function () {
-                    callback.apply(socket, args);
-                });
-            });
-        }
-
-        function emit(eventName, data, callback) {
-            socket.emit(eventName, data, function () {
-                var args = arguments;
-                $rootScope.$apply(function () {
-                    if (callback) {
-                        callback.apply(socket, args);
-                    }
-                });
-            });
-        }
-
-	}
 })();
 
 (function () {
